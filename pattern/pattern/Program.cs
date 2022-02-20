9c8684ba7a038ccd0a02fb5be9565d08a340fae1
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 00:30:32 +0530

    Add files via upload

---
+++ pattern/pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            
+            for(int i=0; i<=5; i++)
+            {
+                Console.WriteLine(  );
+                for(int j=i; j<=5; j++)
+                {
+                    Console.Write("*");
+                }
+                
+            }
+        }
+    }
+}
